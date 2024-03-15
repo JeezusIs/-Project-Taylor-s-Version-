f3ee0133c6a64f34a9f84a69ba0405913b39dbf4
¥eezus (Taylor's Version) <158213403+JeezusIs@users.noreply.github.com>
2024-03-15 13:25:13 +0300

    Update spaceship.h

--- spaceship.h
+++ spaceship.h
@@ -1,59 +1,43 @@
 #pragma once
 #include <iostream>
-
 using namespace std;
 
 
 class Spaceship {
 public:
-	string name;
-	int hp;
-	int fuel;
-	int* pcash;
-	int type;
-	int damagetaken = 33;
-	int hasarKatsayisi1 = damagetaken * 1.5;
-	int hasarKatsayisi2 = damagetaken * 1;
-	int hasarKatsayisi3 = damagetaken * 0.5;
+    Spaceship() = default;
+    Spaceship(int _flee, int _dT) : flee_posibility{ _flee }, dT{ _dT } {};
 
+    inline float getHp() const {
+        return Hp;
+    }
+    inline int getFuel() const {
+        return fuel;
+    }
+    inline int getCash() const {
+        return cash;
+    }
+    inline void setHealth(float _newHp) {
+        Hp = _newHp;
+    }
+    inline void setFuel(int _newFuel) {
+        fuel = _newFuel;
+    }
+    inline void setMoney(int _newCash) {
+        cash = _newCash;
+    }
+    inline float getDamagePossibility() const {
+        return dT;
+    }
+    inline int getEscapePossibility() const {
+        return flee_posibility;
+    }
+    virtual void status() = 0;
 
-	Spaceship(string _name = "", int _hp = 100, int _fuel = 100, int cash = 0, int _type);
-};
-
-
-class HizliGemi : public Spaceship {
-public:
-	int _hp1 = hp;
-	int _fuel1 = fuel;
-	int cash1 = *pcash;
-	int _type;
-	int _hasarKatsayi1;
-
-	HizliGemi(int _hp1, int _fuel1, int cash1, int _type, int _hasarKatsayisi1);
-};
-
-
-
-class DengeliGemi : public Spaceship {
-public:
-	int _hp2 = hp;
-	int _fuel2 = fuel;
-	int cash2 = *pcash;
-	int _type;
-	int _hasarKatsayisi2;
-
-	DengeliGemi(int _hp2, int _fuel2, int cash2, int _type, int _hasarKatsayisi2);
-};
-
-
-
-class GucluGemi : public Spaceship {
-public:
-	int _hp3 = hp;
-	int _fuel3 = fuel;
-	int cash3 = *pcash;
-	int _type;
-	int _hasarKatsayisi3;
-
-	GucluGemi(int _hp3, int _fuel3, int cash3, int _type, int _hasarKatsayisi3);
+protected:
+    float Hp{ 100 };
+    int fuel{ 100 };
+    int cash{ 0 };
+    float dT{ 0 };
+    int flee_posibility{ 0 };
 };
